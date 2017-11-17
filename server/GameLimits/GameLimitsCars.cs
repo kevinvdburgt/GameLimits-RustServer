4fbddc621094a220a646ed02fd6a0cd3402fdab0
Kevin van der Burgt <mail@kevinvdburgt.com>
2017-11-17 01:35:22 +0100

    Fixed issue #1

--- server/GameLimits/GameLimitsCars.cs
+++ server/GameLimits/GameLimitsCars.cs
@@ -1,7 +1,8 @@
 ﻿using static Oxide.Plugins.GameLimits;
-using System.Net;
 using UnityEngine;
 using System.Collections.Generic;
+using System;
+using System.Linq;
 
 namespace Oxide.Plugins
 {
@@ -10,42 +11,74 @@ namespace Oxide.Plugins
 
     public class GameLimitsCars : RustPlugin
     {
+        /// <summary>
+        /// The spawn locations for the cars to spawn.
+        /// </summary>
         private List<Vector3> spawns = new List<Vector3>();
 
-        const string carPrefab = "assets/content/vehicles/sedan_a/sedantest.entity.prefab";
+        /// <summary>
+        /// The car prefab to spawn in the world
+        /// </summary>
+        private static readonly string carPrefab = "assets/content/vehicles/sedan_a/sedantest.entity.prefab";
 
-        void OnServerInitialized()
+        /// <summary>
+        /// A whitelist of monuments where cars can spawn
+        /// </summary>
+        private static readonly string[] spawnWhitelist = {
+            "assets/bundled/prefabs/autospawn/monument/large/launch_site_1.prefab",
+            "assets/bundled/prefabs/autospawn/monument/large/water_treatment_plant_1.prefab",
+            "assets/bundled/prefabs/autospawn/monument/large/trainyard_1.prefab",
+            "assets/bundled/prefabs/autospawn/monument/large/military_tunnel_1.prefab",
+            "assets/bundled/prefabs/autospawn/monument/large/airfield_1.prefab",
+            "assets/bundled/prefabs/autospawn/monument/small/gas_station_1.prefab",
+        };
+
+        /// <summary>
+        /// When the server is initialized, find all car spawns and set the timer
+        /// </summary>
+        private void OnServerInitialized()
         {
             // Find all spawn locations for the car
-            var monuments = UnityEngine.Object.FindObjectsOfType<MonumentInfo>();
-            Vector3 offset = new Vector3(20f, 20f, 20f);
-            foreach (var monument in monuments)
-                spawns.Add(monument.transform.position + offset);
+            foreach (MonumentInfo monument in UnityEngine.Object.FindObjectsOfType<MonumentInfo>())
+                if (spawnWhitelist.Any(s => s.Equals(monument.name)))
+                    spawns.Add(monument.transform.position + new Vector3(20f, 50f, 20f));
 
             // Set the respawn check timer
             timer.Every(60f * 60f, () => Respawn());
             Respawn();
         }
 
+        /// <summary>
+        /// Check all spawn locations if there is a car nearby, if not, spawn a new car.
+        /// </summary>
         void Respawn()
         {
-            Log("Cars", "Checking for cars to respawn", GameLimits.LogType.INFO);
-
+            // Spawn new cars
             foreach (var spawn in spawns)
             {
                 List<BaseCar> entities = Facepunch.Pool.GetList<BaseCar>();
-                Vis.Entities(spawn, 100f, entities);
+                Vis.Entities(spawn, 150f, entities);
 
                 if (entities.Count > 0)
                     continue;
 
-                Log("Cars", $"Spawning new car at {spawn.x} {spawn.y} {spawn.z}", GameLimits.LogType.OK);
-
                 BaseEntity ent = (BaseCar)GameManager.server.CreateEntity(carPrefab, spawn);
                 ent.Spawn();
+
+                Puts($"Spawned new car at {spawn.x} {spawn.y} {spawn.z}");
+
+                Facepunch.Pool.FreeList(ref entities);
+            }
+
+            // Remove cars that are placed under water
+            foreach (BaseCar car in UnityEngine.Object.FindObjectsOfType<BaseCar>())
+            {
+                if (!car.IsMounted() && WaterLevel.Factor(car.WorldSpaceBounds().ToBounds()) > 0.7f)
+                {
+                    Puts($"Despawned car underwater from {car.transform.position.x} {car.transform.position.y} {car.transform.position.z}");
+                    car.DieInstantly();
+                }
             }
         }
     }
 }
-
-// 76561198176419121
